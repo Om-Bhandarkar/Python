d1063c3502d4b031876a50d0dfa773ac11a96abf
Om-Bhandarkar <ombhandarkar1206@gmail.com>
2023-12-23 04:15:05 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Input_Ouput/input11.py
@@ -0,0 +1,22 @@
+x,y,z = [int(data) for data in input("Enter values ").split(" ")]
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+a,b,c = [(data) for data in input("Enter values ").split(" ")]
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
